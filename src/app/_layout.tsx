305a56558de3da53f0426c80a926a82003fe4869
Ololade Olaonipekun <olaonipekunololade94@gmail.com>
2026-04-21 05:56:30 +0100

    macrozine changes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+import { globalStyles } from "@/styles/globals";
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <Stack
+      screenOptions={{
+        headerTitle: "Macrozone",
+        headerShadowVisible: true,
+        headerStyle: {
+          backgroundColor: globalStyles.container.backgroundColor,
+        },
+        headerTintColor: "#FFFFFF",
+        headerTitleStyle: { fontWeight: "bold" },
+        headerTitleAlign: "center",
+      }}
+    />
   );
 }
